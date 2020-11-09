86b9a527e0096dca4aba43026fb25d5bca7443ca
arturshagito0 <art_shagi@mail.ru>
2020-11-09 05:21:04 +0000

    changed to y

--- mlstuff.py
+++ mlstuff.py
@@ -2,11 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 a = np.linspace(0, np.pi * 2, 100)
-b = a * np.exp(-a) - np.tan(a)
+ys = a * np.exp(-a) - np.tan(a)
 
-plt.plot(a, b)
-plt.scatter(a, b, c='r')
+plt.plot(a, ys)
+plt.scatter(a, ys, c='r')
 plt.show()
 
 c = [n ** 2 - 1 for n in range(0, 100)]
-
