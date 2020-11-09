e989615f953ede8d09cf914a8195ec07153b80b9
arturshagito0 <art_shagi@mail.ru>
2020-11-09 05:16:17 +0000

    Removed c // changed function

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from distutils.version import LooseVersion
+import warnings
+import tensorflow as tf
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(tf.version)
+# Check TensorFlow Version
+assert LooseVersion(tf.__version__) >= LooseVersion(
+    '1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+print('TensorFlow Version: {}'.format(tf.__version__))
 
+# Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn('No GPU found. Please ensure you have installed TensorFlow correctly')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(tf.test.gpu_device_name())
--- mlstuff.py
+++ mlstuff.py
@@ -1,13 +1,12 @@
+import numpy as np
 from matplotlib import pyplot as plt
 
-import numpy as np
-
 a = np.linspace(0, np.pi * 2, 100)
-b = a * np.exp(-a)
+b = a * np.exp(-a) - np.tan(a)
 
 plt.plot(a, b)
 plt.scatter(a, b, c='r')
 plt.show()
 
 c = [n ** 2 - 1 for n in range(0, 100)]
-print(c)
+
